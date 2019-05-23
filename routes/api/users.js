7bfabb23dbb3cc38e0d4ce4ce0dd71b98ac0839f
Tanvi Garg <tanvi.garg23@gmail.com>
2019-05-22 23:25:12 -0700

    import express validator for post request validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const  { check, validationResult } = require('express-validator/check');
 
 // @route   POST api/users
 // @desc    Register user
